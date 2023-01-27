f86003fd93aa7b15199e472831f8d20fa24a25b0
Tim0805 <28450@stpauls.qld.edu.au>
2023-01-27 12:45:30 +1000

    .

--- Unit1_Create_With_Code/MyPython01.py
+++ Unit1_Create_With_Code/MyPython01.py
@@ -1,17 +1,18 @@
-# This is a comment 
-***
-This is 
-a multi line 
-comment 
-***
+# This is a comment
+"""
+This is
+a multi line
+comment
+"""
 
-# Input 
-name = input ("what is your name? ") 
-age = input ("what is your age? ")
+# Input
+name = input("what is your name? ")
+age = int(input("what is your age? "))
+print(type(name))
+print(type(age))
 
-# Processing 
-# Python can calculate numbers 
-age = 20 + age 
-
-#  Output 
-print("Hi," name, "your age in 20 years will be", age)
+# Processing
+# Python can calculate numbers
+age = str(20 + age)
+#  Output
+print("Hi, " +name+ " your age in 20 years will be " +age+ ".")
